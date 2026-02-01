4bb824247fbaba02078c030d7a0be92550e3fa4b
Sulaimon <maito4me@gmail.com>
2026-02-01 17:03:19 +0100

    deploy.yaml variables update

--- server.js
+++ server.js
@@ -3,11 +3,10 @@ const connectDB = require("./config/db");
 const app = require("./app");
 const {startOverDueTaskJob} = require("./cron/checkOverdueTasks")
 
-const PORT = process.env.PORT || 5000;
-
 connectDB()
 startOverDueTaskJob()
 
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
